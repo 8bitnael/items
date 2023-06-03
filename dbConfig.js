57939d61e9b923cc22f4ff535a0605c304a1bf79
vdanael <86101748+8bitnael@users.noreply.github.com>
2023-06-03 15:57:55 +0200

    Add files via upload

---
+++ dbConfig.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const itemsPool = new Pool({
+    //connectionString: process.env.DBConfigLink,
+    connectionString: process.env.DBConnLink,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = itemsPool;
